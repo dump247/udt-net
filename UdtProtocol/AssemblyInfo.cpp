e04901fb4350441cc12b86e9d8d07a87a02ba274
Cory Thomas <cthomas7577@gmail.com>
2011-06-23 13:13:03 -0600

    Set the version number to 0.2.0.0

--- UdtProtocol/AssemblyInfo.cpp
+++ UdtProtocol/AssemblyInfo.cpp
@@ -63,7 +63,7 @@ using namespace System::Security::Permissions;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("1.0.*")];
+[assembly:AssemblyVersionAttribute("0.2.0.0")];
 
 [assembly:ComVisible(false)];
 
