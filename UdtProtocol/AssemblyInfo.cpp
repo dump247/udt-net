47ed8b203e6458ba451e100e69167a29dccbf73b
Cory Thomas <cthomas7577@gmail.com>
2011-07-07 14:47:53 -0600

    Updated version number to 0.3.0.0.

--- UdtProtocol/AssemblyInfo.cpp
+++ UdtProtocol/AssemblyInfo.cpp
@@ -48,7 +48,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyConfigurationAttribute("")];
 [assembly:AssemblyCompanyAttribute("")];
 [assembly:AssemblyProductAttribute("Udt")];
-[assembly:AssemblyCopyrightAttribute("Copyright (c)  2010")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c)  2011")];
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
@@ -63,7 +63,7 @@ using namespace System::Security::Permissions;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("0.2.0.0")];
+[assembly:AssemblyVersionAttribute("0.3.0.0")];
 
 [assembly:ComVisible(false)];
 
