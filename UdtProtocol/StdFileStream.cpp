55a3ee81cb0dd615bfe82ae032f3bf8357c16047
Cory Thomas <cthomas7577@gmail.com>
2011-07-15 15:54:03 -0600

    Reworked StdFileStream directly on top of std::fstream. The stream supports almost all FileMode/FileAccess/FileShare options.

--- UdtProtocol/StdFileStream.cpp
+++ UdtProtocol/StdFileStream.cpp
@@ -36,6 +36,7 @@
 #include <iostream>
 #include <io.h>
 #include <fcntl.h>
+#include <vcclr.h>
 
 using namespace Udt;
 using namespace System;
@@ -46,176 +47,499 @@ using namespace Microsoft::Win32::SafeHandles;
 
 StdFileStream::StdFileStream(String^ path, FileMode mode)
 {
-	Init(gcnew FileStream(path, mode));
+	Init(path, mode, mode == FileMode::Append ? FileAccess::Write : FileAccess::ReadWrite, FileShare::None);
 }
 
 StdFileStream::StdFileStream(String^ path, FileMode mode, FileAccess access)
 {
-	Init(gcnew FileStream(path, mode, access));
+	Init(path, mode, access, FileShare::None);
 }
 
 StdFileStream::StdFileStream(String^ path, FileMode mode, FileAccess access, FileShare share)
 {
-	Init(gcnew FileStream(path, mode, access, share));
+	Init(path, mode, access, share);
 }
 
-StdFileStream::StdFileStream(String^ path, FileMode mode, FileAccess access, FileShare share, int bufferSize)
+void StdFileStream::AssertNotDisposed()
 {
-	Init(gcnew FileStream(path, mode, access, share, bufferSize));
+	if (_stdStream == NULL) throw gcnew ObjectDisposedException(this->ToString());
 }
 
-StdFileStream::StdFileStream(String^ path, FileMode mode, FileAccess access, FileShare share, int bufferSize, FileOptions options)
+void StdFileStream::Init(String^ path, FileMode mode, FileAccess access, FileShare share)
 {
-	Init(gcnew FileStream(path, mode, access, share, bufferSize, options));
-}
+	if (path == nullptr) throw gcnew ArgumentNullException("path");
 
-StdFileStream::StdFileStream(String^ path, FileMode mode, FileAccess access, FileShare share, int bufferSize, bool useAsync)
-{
-	Init(gcnew FileStream(path, mode, access, share, bufferSize, useAsync));
-}
+	_canRead = false;
+	_canWrite = false;
+	_canSeek = false;
 
-StdFileStream::StdFileStream(String^ path, FileMode mode, FileSystemRights rights, FileShare share, int bufferSize, FileOptions options, FileSecurity^ fileSecurity)
-{
-	Init(gcnew FileStream(path, mode, rights, share, bufferSize, options, fileSecurity));
-}
+	int shareFlag;
+	pin_ptr<const wchar_t> pathPin = PtrToStringChars(path);
+	FILE* streamPtr = NULL;
 
-StdFileStream::StdFileStream(String^ path, FileMode mode, FileSystemRights rights, FileShare share, int bufferSize, FileOptions options)
-{
-	Init(gcnew FileStream(path, mode, rights, share, bufferSize, options));
-}
+	switch (share)
+	{
+	case FileShare::Read:
+		shareFlag = _SH_DENYWR;
+		break;
 
-StdFileStream::StdFileStream(SafeFileHandle^ handle, FileAccess access)
-{
-	Init(gcnew FileStream(handle, access));
-}
+	case FileShare::Write:
+		shareFlag = _SH_DENYRD;
+		break;
 
-StdFileStream::StdFileStream(SafeFileHandle^ handle, FileAccess access, int bufferSize)
-{
-	Init(gcnew FileStream(handle, access, bufferSize));
-}
+	case FileShare::ReadWrite:
+		shareFlag = _SH_DENYNO;
+		break;
 
-StdFileStream::StdFileStream(SafeFileHandle^ handle, FileAccess access, int bufferSize, bool isAsync)
-{
-	Init(gcnew FileStream(handle, access, bufferSize, isAsync));
-}
+	case FileShare::None:
+		shareFlag = _SH_DENYRW;
+		break;
 
-void StdFileStream::Init(System::IO::FileStream^ fileStream)
-{
-	_fileStream = fileStream;
-	_stdStream = NULL;
+	default:
+		throw gcnew ArgumentOutOfRangeException("share", share, "Share mode is not supported.");
+	}
+
+	switch (mode)
+	{
+	case FileMode::CreateNew:
+		streamPtr = InitCreateNew(pathPin, access, shareFlag);
+		break;
+		
+	case FileMode::Create:
+		streamPtr = InitCreate(pathPin, access, shareFlag);
+		break;
+
+	case FileMode::Open:
+		streamPtr = InitOpen(pathPin, access, shareFlag);
+		break;
+		
+	case FileMode::OpenOrCreate:
+		InitOpenOrCreate(pathPin, access, shareFlag);
+		break;
+		
+	case FileMode::Truncate:
+		streamPtr = InitTruncate(pathPin, access, shareFlag);
+		break;
+		
+	case FileMode::Append:
+		streamPtr = InitAppend(pathPin, access, shareFlag);
+		break;
+
+	default:
+		throw gcnew ArgumentOutOfRangeException("mode", mode, "File mode is not supported.");
+	}
+
+	_stdStream = new std::fstream(streamPtr);
+	_stdStream->exceptions(std::ios::failbit);
 }
 
 StdFileStream::~StdFileStream(void)
 {
-	_fileStream->Close();
+	_canRead = false;
+	_canWrite = false;
+	_canSeek = false;
 
 	delete _stdStream;
 	_stdStream = NULL;
 }
 
-std::fstream& StdFileStream::LoadStdStream(void)
+void StdFileStream::CheckLastError(const wchar_t* path)
 {
-	if (_stdStream == NULL)
+	if (errno == ENOENT)
 	{
-		// SafeFileHandle will flush the stream
-		SafeFileHandle^ fileHandle = _fileStream->SafeFileHandle;
-
-		// Get file descriptor
-		int fileDescFlags = _O_RDONLY;
-
-		if (_fileStream->CanWrite)
-		{
-			if (_fileStream->CanRead)
-				fileDescFlags = _O_RDWR;
-			else
-				fileDescFlags = _O_WRONLY;
-		}
-
-		int fileDesc = _open_osfhandle((intptr_t)fileHandle->DangerousGetHandle(), fileDescFlags);
-
-		if (fileDesc == -1) throw gcnew IOException("Error opening C run-time file descriptor.");
-
-		// Get file stream
-		FILE* filePtr;
-
-		switch (fileDescFlags)
-		{
-		case _O_RDONLY:
-			filePtr = _fdopen(fileDesc, "r");
-			break;
-			
-		case _O_WRONLY:
-			filePtr = _fdopen(fileDesc, "w");
-			break;
-			
-		case _O_RDWR:
-			filePtr = _fdopen(fileDesc, "r+");
-			break;
-		}
-
-		// Get std::fstream
-		_stdStream = new std::fstream(filePtr);
+		throw gcnew FileNotFoundException("File not found.", gcnew String(path));
 	}
 	else
 	{
-		_fileStream->Flush();
+		char buffer[1024];
+		strerror_s(buffer, 1024, errno);
+		throw gcnew IOException(gcnew String(buffer));
+	}
+}
+
+void StdFileStream::CheckFileNotExists(const wchar_t* path, int shareFlag)
+{
+	struct _stat statInfo;
+
+	// Throw an error if anything other than file not found occurs
+
+	if (_wstat(path, &statInfo) == 0)
+	{
+		throw gcnew IOException("File already exists");
+	}
+	else if (errno != ENOENT)
+	{
+		char buffer[1024];
+		strerror_s(buffer, 1024, errno);
+		throw gcnew IOException(gcnew String(buffer));
+	}
+}
+
+FILE* StdFileStream::InitCreateNew(const wchar_t* path, FileAccess access, int shareFlag)
+{
+	FILE* streamPtr;
+
+	// There is a race condition here in that the first operation is to
+	// check if the file exists before attempting to open for reading.
+	// It is possible for another process to create the file between these
+	// two operations. Risk is small; not sure how to handle that scenario, though.
+
+	switch (access)
+	{
+	case FileAccess::ReadWrite:
+		CheckFileNotExists(path, shareFlag);
+
+		streamPtr = _wfsopen(path, L"wbN+", shareFlag);
+		if (streamPtr == NULL) CheckLastError(path);
+
+		_canRead = true;
+		_canWrite = true;
+		_canSeek = true;
+		break;
+
+	case FileAccess::Write:
+		CheckFileNotExists(path, shareFlag);
+
+		streamPtr = _wfsopen(path, L"wbN", shareFlag);
+		if (streamPtr == NULL) CheckLastError(path);
+
+		_canRead = false;
+		_canWrite = true;
+		_canSeek = true;
+		break;
+
+	default:
+		throw gcnew ArgumentOutOfRangeException("access", access, "Invalid value in combination with FileMode.Create");
 	}
 
-	_stdStream->seekg(_fileStream->Position);
+	return streamPtr;
+}
+		
+FILE* StdFileStream::InitCreate(const wchar_t* path, FileAccess access, int shareFlag)
+{
+	FILE* streamPtr;
+
+	switch (access)
+	{
+	case FileAccess::ReadWrite:
+		streamPtr = _wfsopen(path, L"wbN+", shareFlag);
+		if (streamPtr == NULL) CheckLastError(path);
+
+		_canRead = true;
+		_canWrite = true;
+		_canSeek = true;
+		break;
+
+	case FileAccess::Write:
+		streamPtr = _wfsopen(path, L"wbN", shareFlag);
+		if (streamPtr == NULL) CheckLastError(path);
+
+		_canRead = false;
+		_canWrite = true;
+		_canSeek = true;
+		break;
+
+	default:
+		throw gcnew ArgumentOutOfRangeException("access", access, "Invalid value in combination with FileMode.Create");
+	}
+
+	return streamPtr;
+}
+
+FILE* StdFileStream::InitOpen(const wchar_t* path, FileAccess access, int shareFlag)
+{
+	FILE* streamPtr;
+
+	switch (access)
+	{
+	case FileAccess::ReadWrite:
+	case FileAccess::Write:
+		streamPtr = _wfsopen(path, L"rbN+", shareFlag);
+		if (streamPtr == NULL) CheckLastError(path);
+
+		_canRead = (access == FileAccess::ReadWrite);
+		_canWrite = true;
+		_canSeek = true;
+		break;
+
+	case FileAccess::Read:
+		streamPtr = _wfsopen(path, L"rbN", shareFlag);
+		if (streamPtr == NULL) CheckLastError(path);
+
+		_canRead = true;
+		_canWrite = false;
+		_canSeek = true;
+		break;
+
+	default:
+		throw gcnew ArgumentOutOfRangeException("access", access, "Invalid value in combination with FileMode.Open");
+	}
+
+	return streamPtr;
+}
+		
+FILE* StdFileStream::InitOpenOrCreate(const wchar_t* path, FileAccess access, int shareFlag)
+{
+	FILE* streamPtr;
+
+	switch (access)
+	{
+	case FileAccess::ReadWrite:
+	case FileAccess::Write:
+		streamPtr = _wfsopen(path, L"rbN+", shareFlag);
+
+		if (streamPtr == NULL)
+		{
+			if (errno == ENOENT) streamPtr = _wfsopen(path, L"wbN+", shareFlag);
+			if (streamPtr == NULL) CheckLastError(path);
+		}
+
+		_canRead = (access == FileAccess::ReadWrite);
+		_canWrite = true;
+		_canSeek = true;
+		break;
+
+	case FileAccess::Read:
+		streamPtr = _wfsopen(path, L"rbN", shareFlag);
+
+		if (streamPtr == NULL)
+		{
+			if (errno == ENOENT) streamPtr = _wfsopen(path, L"wbN", shareFlag);
+			if (streamPtr == NULL) CheckLastError(path);
+		}
+
+		_canRead = true;
+		_canWrite = false;
+		_canSeek = true;
+		break;
+
+	default:
+		throw gcnew ArgumentOutOfRangeException("access", access, "Invalid value in combination with FileMode.OpenOrCreate");
+	}
+
+	return streamPtr;
+}
+		
+FILE* StdFileStream::InitTruncate(const wchar_t* path, FileAccess access, int shareFlag)
+{
+	FILE* streamPtr;
+
+	switch (access)
+	{
+	case FileAccess::ReadWrite:
+	case FileAccess::Write:
+		streamPtr = _wfsopen(path, L"rbN+", shareFlag);
+
+		if (streamPtr == NULL)
+			CheckLastError(path);
+
+		if (_chsize(_fileno(streamPtr), 0) != 0)
+		{
+			fclose(streamPtr);
+			CheckLastError(path);
+		}
+
+		_canRead = (access == FileAccess::ReadWrite);
+		_canWrite = true;
+		_canSeek = true;
+		break;
+
+	default:
+		throw gcnew ArgumentOutOfRangeException("access", access, "Invalid value in combination with FileMode.Truncate");
+	}
+
+	return streamPtr;
+}
+		
+FILE* StdFileStream::InitAppend(const wchar_t* path, FileAccess access, int shareFlag)
+{
+	FILE* streamPtr;
+
+	switch (access)
+	{
+	case FileAccess::Write:
+		streamPtr = _wfsopen(path, L"abN", shareFlag);
+		if (streamPtr == NULL) CheckLastError(path);
+
+		_canRead = false;
+		_canWrite = true;
+		_canSeek = false;
+		break;
+
+	default:
+		throw gcnew ArgumentOutOfRangeException("access", access, "Invalid value in combination with FileMode.Append");
+	}
+
+	return streamPtr;
+}
+
+std::fstream& StdFileStream::LoadStdStream(void)
+{
+	AssertNotDisposed();
 	return *_stdStream;
 }
 
 void StdFileStream::Flush(void)
 {
-	_fileStream->Flush();
+	AssertNotDisposed();
+	if (CanWrite) _stdStream->flush();
 }
 
 bool StdFileStream::CanRead::get(void)
 {
-	return _fileStream->CanRead;
+	return _canRead;
 }
 
 bool StdFileStream::CanSeek::get(void)
 {
-	return _fileStream->CanSeek;
+	return _canSeek;
 }
 
 bool StdFileStream::CanWrite::get(void)
 {
-	return _fileStream->CanWrite;
+	return _canWrite;
 }
 
 __int64 StdFileStream::Length::get(void)
 {
-	return _fileStream->Length;
+	AssertNotDisposed();
+
+	try
+	{
+		std::streampos currentPos = _stdStream->tellg();
+		_stdStream->seekg(0, std::ios_base::end);
+		std::streampos length = _stdStream->tellg();
+		_stdStream->seekg(currentPos);
+
+		return length;
+	}
+	catch (const std::exception& ex)
+	{
+		throw gcnew IOException(gcnew String(ex.what()));
+	}
 }
 
 __int64 StdFileStream::Position::get(void)
 {
-	return _fileStream->Position;
+	AssertNotDisposed();
+
+	try
+	{
+		return _stdStream->tellg();
+	}
+	catch (const std::exception& ex)
+	{
+		throw gcnew IOException(gcnew String(ex.what()));
+	}
 }
 
 void StdFileStream::Position::set(__int64 value)
 {
-	_fileStream->Position = value;
+	AssertNotDisposed();
+	
+	// The documentation for FileMode::Append states that attempting to seek
+	// results in IOException while attempting to read results in NotSupportedException
+	if (!CanSeek) throw gcnew IOException("Stream does not support seeking.");
+
+	try
+	{
+		_stdStream->seekg(value);
+	}
+	catch (const std::exception& ex)
+	{
+		throw gcnew IOException(gcnew String(ex.what()));
+	}
 }
 
 void StdFileStream::SetLength(__int64 value)
 {
-	_fileStream->SetLength(value);
+	throw gcnew NotImplementedException();
 }
 
 __int64 StdFileStream::Seek(__int64 offset, SeekOrigin origin)
 {
-	return _fileStream->Seek(offset, origin);
+	AssertNotDisposed();
+
+	// The documentation for FileMode::Append states that attempting to seek
+	// results in IOException while attempting to read results in NotSupportedException
+	if (!CanSeek) throw gcnew IOException("Stream does not support seeking.");
+
+	try
+	{
+		switch (origin)
+		{
+		case SeekOrigin::Begin:
+			_stdStream->seekg(offset, std::ios_base::beg);
+			break;
+		
+		case SeekOrigin::End:
+			_stdStream->seekg(offset, std::ios_base::end);
+			break;
+
+		case SeekOrigin::Current:
+			_stdStream->seekg(offset, std::ios_base::cur);
+			break;
+
+		default:
+			throw gcnew ArgumentOutOfRangeException("origin", origin, "Unsupported value.");
+		}
+	}
+	catch (const std::exception& ex)
+	{
+		throw gcnew IOException(gcnew String(ex.what()));
+	}
+
+	return Position;
 }
 
 int StdFileStream::Read(cli::array<unsigned char>^ buffer, int offset, int count)
 {
-	return _fileStream->Read(buffer, offset, count);
+	if (buffer == nullptr) throw gcnew ArgumentNullException("buffer");
+	if (offset < 0) throw gcnew ArgumentOutOfRangeException("offset", offset, "Value must be greater than or equal to 0.");
+	if (count < 0) throw gcnew ArgumentOutOfRangeException("count", count, "Value must be greater than or equal to 0.");
+	if (buffer->Length - offset < count) throw gcnew ArgumentException("Invalid array offset/count.");
+	
+	AssertNotDisposed();
+
+	if (!CanRead) throw gcnew NotSupportedException("Stream does not support reading.");
+
+	if (_stdStream->eof()) return 0;
+
+	pin_ptr<unsigned char> bufferPin = &buffer[0];
+    unsigned char* bufferPtr = bufferPin;
+
+    try
+    {
+		_stdStream->read((char*)bufferPtr + offset, count);
+    }
+    catch (const std::exception& ex)
+    {
+		if (_stdStream->bad()) throw gcnew IOException(gcnew String(ex.what()));
+    }
+
+    return (int)_stdStream->gcount();
 }
 
 void StdFileStream::Write(cli::array<unsigned char>^ buffer, int offset, int count)
 {
-	_fileStream->Write(buffer, offset, count);
+	if (buffer == nullptr) throw gcnew ArgumentNullException("buffer");
+	if (offset < 0) throw gcnew ArgumentOutOfRangeException("offset", offset, "Value must be greater than or equal to 0.");
+	if (count < 0) throw gcnew ArgumentOutOfRangeException("count", count, "Value must be greater than or equal to 0.");
+	if (buffer->Length - offset < count) throw gcnew ArgumentException("Invalid array offset/count.");
+	
+	AssertNotDisposed();
+
+	if (!CanWrite) throw gcnew NotSupportedException("Stream does not support writing.");
+
+	pin_ptr<unsigned char> bufferPin = &buffer[0];
+    unsigned char* bufferPtr = bufferPin;
+
+    try
+    {
+		_stdStream->write((char*)bufferPtr + offset, count);
+    }
+    catch (const std::exception& ex)
+    {
+		throw gcnew IOException(gcnew String(ex.what()));
+    }
 }
