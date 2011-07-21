30b1d03e64d4375d6a89647f2ffd30ac99e05502
Cory Thomas <cthomas7577@gmail.com>
2011-07-21 10:54:41 -0600

    Added ShutdownPacket.

---
+++ UdtProtocol-Test/ShutdownPacketTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Udt;
+
+namespace UdtProtocol_Test
+{
+	/// <summary>
+	/// Test fixture for <see cref="ShutdownPacket"/>.
+	/// </summary>
+	[TestFixture]
+	public class ShutdownPacketTest
+	{
+		[Test]
+		public void Create_and_dispose()
+		{
+			ShutdownPacket packet = new ShutdownPacket();
+			
+			Assert.AreEqual(0, packet.DestinationId);
+			Assert.IsFalse(packet.IsDisposed);
+			Assert.IsTrue(packet.IsEditable);
+			Assert.AreEqual(TimeSpan.Zero, packet.TimeStamp);
+
+			packet.Dispose();
+
+			Assert.Throws<ObjectDisposedException>(() => { var x = packet.DestinationId; });
+			Assert.IsTrue(packet.IsDisposed);
+			Assert.IsFalse(packet.IsEditable);
+			Assert.Throws<ObjectDisposedException>(() => { var x = packet.TimeStamp; });
+		}
+	}
+}
--- UdtProtocol/ShutdownPacket.cpp
+++ UdtProtocol/ShutdownPacket.cpp
@@ -31,101 +31,20 @@
  ****************************************************************/
 
 #include "StdAfx.h"
-
-#include "Packet.h"
-#include "DataPacket.h"
-#include "ControlPacket.h"
-#include "KeepAlivePacket.h"
 #include "ShutdownPacket.h"
 
 #include <udt.h>
 #include <packet.h>
 
 using namespace Udt;
-using namespace System;
 
-Packet^ Packet::Wrap(const CPacket* packet)
+ShutdownPacket::ShutdownPacket(CPacket* packet, bool deletePacket)
+	: ControlPacket(packet, deletePacket)
 {
-	Packet^ wrapper;
-
-	if (packet->getFlag())
-	{
-		switch (packet->getType())
-		{
-		case ShutdownPacket::TypeCode:
-			wrapper = gcnew ShutdownPacket((CPacket*)packet, false);
-			break;
-
-		case KeepAlivePacket::TypeCode:
-			wrapper = gcnew KeepAlivePacket((CPacket*)packet, false);
-			break;
-
-		default:
-			wrapper = gcnew ControlPacket((CPacket*)packet, false);
-			break;
-		}
-	}
-	else
-	{
-		wrapper = gcnew DataPacket((CPacket*)packet, false);
-	}
-
-	return wrapper;
 }
 
-Packet::Packet(CPacket* packet, bool deletePacket)
+ShutdownPacket::ShutdownPacket(void)
+	: ControlPacket(new CPacket(), true)
 {
-	_packet = packet;
-	_deletePacket = deletePacket;
-}
-
-Packet::~Packet()
-{
-	if (_deletePacket)
-	{
-		FreePacketData();
-		delete _packet;
-	}
-
-	_packet = NULL;
-}
-
-void Packet::AssertNotDisposed()
-{
-	if (_packet == NULL)
-		throw gcnew ObjectDisposedException(this->ToString());
-}
-
-void Packet::AssertIsMutable()
-{
-	AssertNotDisposed();
-	if (!IsEditable) throw gcnew InvalidOperationException("Packet can not be modified.");
-}
-
-TimeSpan Packet::TimeStamp::get(void)
-{
-	AssertNotDisposed();
-	return FromMicroseconds((uint32_t)_packet->m_iTimeStamp);
-}
-
-void Packet::TimeStamp::set(TimeSpan value)
-{
-	AssertIsMutable();
-
-	if (value < TimeSpan::Zero || value > MaxTimeStamp)
-		throw gcnew ArgumentOutOfRangeException("value", value, String::Concat("Value must be between ", TimeSpan::Zero, " and ", MaxTimeStamp, "."));
-
-	_packet->m_iTimeStamp = (int32_t)ToMicroseconds(value);
-}
-
-int Packet::DestinationId::get(void)
-{
-	AssertNotDisposed();
-	return _packet->m_iID;
-}
-
-void Packet::DestinationId::set(int value)
-{
-	AssertIsMutable();
-	_packet->m_iID = value;
+	_packet->pack(TypeCode, NULL, NULL);
 }
